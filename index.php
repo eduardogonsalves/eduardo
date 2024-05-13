4f341d764b79884af4b341a82adf8f7ce268d26a
eduardogonsalves <c.edugonsalves@gmail.com>
2024-05-13 15:28:56 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doc PHP</title>
+</head>
+
+<body>
+
+<?php
+    $nome = "Olá Mundo!";
+    echo "<h1>$nome</h1>";
+?>
+
+
+<?php
+
+   function media($nota1, $nota2, $nota3){
+        $resultadoFinal = ($nota1 + $nota2 + $nota3) / 3;
+        return $resultadoFinal;
+   }
+    echo "Média Aritimética = ";
+    echo (media(8.9, 9.2, 7.4));
+
+?>
+
+<br><br><br>
+
+<?php
+
+   function mediaPonderada($nota1, $nota2, $nota3){
+        $resultadoFinal = ($nota1*1 + $nota2*4 + $nota3*6) / 11;
+        return $resultadoFinal;
+   }
+    echo "Média Ponderada = ";
+    echo (mediaPonderada(8.9, 9.2, 7.4));
+
+?>
+
+
+</body>
+
+</html>
